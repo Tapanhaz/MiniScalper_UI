4f861e21a96ed91774783673bfed5171527dda70
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2023-01-17 21:54:47 +0530

    Update mini_scalper.py
    Strike generation updated .. Have to change any of the combo boxes value after login to get live price ..

--- mini_scalper.py
+++ mini_scalper.py
@@ -1,10 +1,18 @@
-import sys
+import sys,yaml,pyotp,threading,requests,configparser
+from NorenRestApiPy.NorenApi import  NorenApi
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QEvent
+from time import sleep
+import pandas as pd, numpy as np
+from io import BytesIO
+from zipfile import ZipFile
+from datetime import datetime, timedelta
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("main.ui")
 Ui_SecondWindow, _ = uic.loadUiType("sub.ui")
 Ui_ThirdWindow, _ = uic.loadUiType("sl.ui")
+
+
 
 class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdWindow):
     def __init__(self):
@@ -17,6 +25,9 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
         self.btn_menu.clicked.connect(self.create_login_window)
         self.btn_exit.clicked.connect(self.close_windows)
         self.btn_minimize.clicked.connect(self.minimize_windows)
+
+        self.var_token = ""
+        self.var_ltp = 0
 
         # window position 
         self.login_window = None
@@ -38,6 +49,22 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
         
         self.frame_title.installEventFilter(self)
         self.installEventFilter(self)
+
+        self.load_expirylist()
+        self.combo_ticker.addItems(['BANKNIFTY','NIFTY'])
+        self.combo_strike.addItems(['ATM', 'ITM','ITM1','ITM2','ITM3','ITM4','OTM','OTM1','OTM2','OTM3','OTM4'])
+        self.combo_lots.addItems(['1','2','3','4','5','6','7','8','9','10'])
+        self.combo_side.addItems(['CE','PE'])
+
+        self.load_settings()
+
+        ## Get combo values 
+
+        self.combo_ticker.currentTextChanged.connect(self.update_var_token)
+        self.combo_strike.currentTextChanged.connect(self.update_var_token)
+        self.combo_lots.currentTextChanged.connect(self.update_var_token)
+        self.combo_side.currentTextChanged.connect(self.update_var_token)
+        self.combo_expiry.currentTextChanged.connect(self.update_var_token)
     
     
     def create_login_window(self):
@@ -51,6 +78,10 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
             self.login_window.show()
             
             self.login_window.installEventFilter(self)
+
+            self.ui_second_window.username.setText("")
+            self.ui_second_window.login_stat.setText("")
+
             self.ui_second_window.btn_menu.setMenu(QtWidgets.QMenu(self.ui_second_window.btn_menu))
             self.ui_second_window.btn_menu.menu().addAction("Always on top", self.toggle_always_on_top).setCheckable(True)
             
@@ -73,6 +104,8 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
             self.ui_second_window.btn_menu.menu().addAction("Get Expiry", self.get_expiry_dates)
             self.ui_second_window.btn_menu.menu().addAction("Save Setings", self.save_settings)
             self.ui_second_window.btn_menu.menu().addAction("Restore Defaults", self.default_settings)
+
+            self.ui_second_window.btn_login.clicked.connect(self.ShoonyaLogin)
 
             if self.sl_window is None:
                 self.sl_window = QtWidgets.QMainWindow()
@@ -102,6 +135,7 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
             self.login_window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint, False)
         self.show()
         self.login_window.show()
+        self.sl_window.show()
 
     def set_default_position(self):
         desktop = QtWidgets.QDesktopWidget()
@@ -196,8 +230,9 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
         border-radius: 6px;
         }
     """)
-
+        
         self.transparency_slider.setVisible(True)
+        
         
     def eventFilter(self, object, event):
         if object == self.transparency_slider:
@@ -252,17 +287,15 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
         geo = self.geometry()
         
         if self.right_top_corner.isChecked() or self.left_top_corner.isChecked():
-            geo.moveBottom(geo.bottom()+(geo.height()+1))
+            geo.moveBottom(geo.bottom()+(geo.height()))
             self.sl_window.setGeometry(geo)
         elif self.right_bottom_corner.isChecked() or self.left_bottom_corner.isChecked():
             height = self.sl_window.size().height()
             geo.moveTop(geo.top() - height)
             self.sl_window.setGeometry(geo)
         else:
-            geo.moveBottom(geo.bottom()+(geo.height()+1))
+            geo.moveBottom(geo.bottom()+(geo.height()))
             self.sl_window.setGeometry(geo)
-
-        
     
     def close_windows(self):
         self.close()
@@ -295,15 +328,230 @@ class MiniScalper(QtWidgets.QMainWindow, Ui_MainWindow,Ui_SecondWindow,Ui_ThirdW
                 self.position_sl_window()
 
     def default_settings(self):
-        pass
+        self.combo_ticker.setCurrentText("BANKNIFTY")
+        self.combo_strike.setCurrentText("ITM")
+        self.combo_lots.setCurrentText("1")
+        self.combo_side.setCurrentText("CE")
+        expiry_dates = [self.combo_expiry.itemText(i) for i in range(self.combo_expiry.count())]
+        current_date = datetime.now().date()
+        expiry_dates = [datetime.strptime(date, '%d-%b-%Y').date() for date in expiry_dates]
+        closest_expiry_date = min(expiry_dates, key=lambda date: abs(date - current_date) if date >= current_date else timedelta.max)
+        self.combo_expiry.setCurrentText(closest_expiry_date.strftime('%d-%b-%Y'))
+        self.save_settings()
 
     def save_settings(self):
-        pass
+        config = configparser.ConfigParser()
+        config['Settings'] = {'ticker': self.combo_ticker.currentText(),
+                             'strike': self.combo_strike.currentText(),
+                             'lots': self.combo_lots.currentText(),
+                             'side': self.combo_side.currentText(),
+                             'expiry': self.combo_expiry.currentText()}
+        with open('settings.ini', 'w') as configfile:
+            config.write(configfile)
+    
+    def load_settings(self):
+        config = configparser.ConfigParser()
+        config.read('settings.ini')
+        if 'Settings' in config:
+            self.combo_ticker.setCurrentText(config['Settings']['ticker'])
+            self.combo_strike.setCurrentText(config['Settings']['strike'])
+            self.combo_lots.setCurrentText(config['Settings']['lots'])
+            self.combo_side.setCurrentText(config['Settings']['side'])
+            #self.combo_expiry.setCurrentText(config['Settings']['expiry'])
+    
+    def update_var_token(self):
+        global ord_qty,nifty_ltp,banknifty_ltp,idx_df,api
+
+        ticker=self.combo_ticker.currentText()
+        strikepos=self.combo_strike.currentText()
+        lots=self.combo_lots.currentText()
+        optype=self.combo_side.currentText()
+        expiry=self.combo_expiry.currentText()
+        if ticker == 'NIFTY':
+            lotsize=50
+            strikediff=50
+            inst_ltp=float(nifty_ltp)
+        elif ticker == 'BANKNIFTY':
+            lotsize=25
+            strikediff=100
+            inst_ltp=float(banknifty_ltp)
+
+        atm_strike = round(inst_ltp/strikediff) * strikediff
+        ord_qty=lotsize*int(lots)
+        self.lbl_qty.setText(str(ord_qty))
+
+        if strikepos == 'ATM':
+            mystrike = atm_strike
+        elif strikepos == "ITM" or strikepos == "OTM":
+            strikepos += "0"
+            pos = int(strikepos[-1]) + 1
+            if optype == "CE":
+                middle_operand = "-"
+            elif optype == "PE":
+                middle_operand = "+"
+            mystrike = eval(f"atm_strike {middle_operand} pos*strikediff")
+            #print(mystrike)
+        else:
+            pos = int(strikepos[-1]) + 1
+            if optype == "CE":
+                middle_operand = "-"
+            elif optype == "PE":
+                middle_operand = "+"
+            mystrike = eval(f"atm_strike {middle_operand} pos*strikediff")
+            #print(mystrike)
+        
+        expiry_date = datetime.strptime(expiry, '%d-%b-%Y').strftime("%d%b%y").upper()
+        
+        trading_symbol=f"{ticker}{expiry_date}{optype[0]}{mystrike}"
+        
+        
+        if float(mystrike) > 10000 :
+            self.var_token = idx_df.loc[idx_df['TradingSymbol'] == trading_symbol, 'Token'].values[0]
+            self.lbl_strike.setText(str(mystrike))
+            #print(self.var_token)
+            api.subscribe(f'NFO|{self.var_token}')
+        else:
+            self.lbl_strike.setText("Error !!")
+    
+     
+
+    def load_expirylist(self):
+        config = configparser.ConfigParser()
+        config.read('expiry.ini')
+        expiry_dates = config.items('Expiry')
+        self.combo_expiry.clear()
+        for date in expiry_dates:
+            self.combo_expiry.addItem(date[1])
+        current_date = datetime.now().date()
+        expiry_dates = [datetime.strptime(date[1], '%d-%b-%Y').date() for date in expiry_dates]
+        closest_expiry_date = min(expiry_dates, key=lambda date: abs(date - current_date) if date >= current_date else timedelta.max)
+        self.combo_expiry.setCurrentText(closest_expiry_date.strftime('%d-%b-%Y'))
 
     def get_expiry_dates(self):
-        pass
+        global api,idx_df
+        nfozip="NFO_symbols.txt.zip"
+        instruments_url=f"https://shoonya.finvasia.com/{nfozip}"
+        res=requests.get(f"{instruments_url}", allow_redirects=True)
+        if res.status_code == 200:
+            zfile = BytesIO(res.content)
+            with ZipFile(zfile, "r") as zip_ref:
+                for file in zip_ref.infolist():
+                    with zip_ref.open(file) as f:
+                        inst_df = pd.read_csv(f)
+                        idx_df= (inst_df.query("Symbol in ['BANKNIFTY', 'NIFTY']")).reset_index(drop=True)
+                        idx_df.to_csv("Index.csv")
+                        expiry_list =np.sort(pd.to_datetime((inst_df[inst_df['Symbol']=='BANKNIFTY'])['Expiry'],format='%d-%b-%Y', errors='coerce').dt.date.unique())
+                        recent_dates = expiry_list[:4]
+
+                        config = configparser.ConfigParser()
+                        config.add_section('Expiry')
+
+                        for i, date in enumerate(recent_dates):
+                            config.set('Expiry', f'Date{i+1}', date.strftime('%d-%b-%Y'))
+
+                        with open('expiry.ini', 'w') as expiry_ini:
+                            config.write(expiry_ini)
+                        self.load_expirylist()
+        else:
+            pass
+        
+    
+    def liveprice(self):
+        global api,nifty_ltp,banknifty_ltp
+        feed_opened = False
+        feedJson = {}
+        orderJson={}
+        
+        banknifty='26009'
+        nifty='26000'
+        bnftoken=f'NSE|{banknifty}'
+        nftoken=f'NSE|{nifty}'
+        
+
+        def event_handler_feed_update(message):
+            if (('lp' in message)&('tk' in message)):
+                feedJson[message['tk']]={'ltp':float(message['lp'])}
+
+
+        def event_handler_order_update(inmessage):
+            if (('norenordno' in inmessage)&('status' in inmessage)):
+                orderJson[inmessage['norenordno']]={'status':float(inmessage['status'])} 
+
+
+        def open_callback():
+            global feed_opened
+            feed_opened = True
+
+        def setup_websocket():
+            global feed_opened,api
+            api.start_websocket( order_update_callback=event_handler_order_update,
+                                 subscribe_callback=event_handler_feed_update, 
+                                 socket_open_callback=open_callback)
+            sleep(1)
+            while(feed_opened==False):
+
+                pass
+            return True
+
+        setup_websocket()
+        api.subscribe(bnftoken)
+        api.subscribe(nftoken)
+
+        
+        
+        #api.subscribe(f'NFO|{nfo_token}')
+        
+        while True:
+            
+            if banknifty in feedJson:
+                self.lbl_banknifty.setText(str(feedJson[banknifty]['ltp']))
+                banknifty_ltp = feedJson[banknifty]['ltp']
+            if nifty in feedJson:
+                self.lbl_nifty.setText(str(feedJson[nifty]['ltp']))
+                nifty_ltp = feedJson[nifty]['ltp']
+            if str(self.var_token) in feedJson:
+                self.lbl_ltp.setText(str(feedJson[str(self.var_token)]['ltp']))
+                self.var_ltp=feedJson[str(self.var_token)]['ltp']
+            sleep(.5)
+
+
+    def ShoonyaLogin(self):
+        global api, login_status
+        with open('cred.yml') as f:
+            cred = yaml.load(f, Loader=yaml.FullLoader)
+            #print(cred)
+        class ShoonyaApiPy(NorenApi):
+            def __init__(self):
+                NorenApi.__init__(self, host='https://api.shoonya.com/NorenWClientTP/', websocket='wss://api.shoonya.com/NorenWSTP/', eodhost='https://api.shoonya.com/chartApi/getdata/')
+        api = ShoonyaApiPy()
+        ret = api.login(userid=cred['user'],
+                        password=cred['pwd'],
+                        twoFA=pyotp.TOTP(cred['token']).now(),
+                        vendor_code=cred['vc'],
+                        api_secret=cred['apikey'],
+                        imei=cred['imei'])
+        login_status=ret['stat']
+        thread_liveprice = threading.Thread(target=self.liveprice, daemon=True)
+
+        if ret['stat'] == 'Ok' :
+            self.ui_second_window.username.setText(ret['uname'].split()[0])
+            self.ui_second_window.login_stat.setText(ret['stat'])
+            thread_liveprice.start()
+            
+        else:
+            self.ui_second_window.login_stat.setText('Error!!')
+        return api
+
+    
 
 if __name__ == '__main__':
+    global login_status, feed_opened, feedJson,api,nifty_ltp,banknifty_ltp,idx_df
+    login_status=""
+    feed_opened=False
+    feedJson={}
+    nifty_ltp="0"
+    banknifty_ltp="0"
+    idx_df=pd.read_csv('Index.csv')
     app = QtWidgets.QApplication(sys.argv)
     window = MiniScalper()
     window.show()
